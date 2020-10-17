b53123fbedd5de6093577d26ead10441807ef303
Sancho2003 <saneksuper178@yandex.ru>
2020-10-17 20:45:07 +0300

    'Fix1'

--- Program 2.py
+++ Program 2.py
@@ -1,10 +1,10 @@
 import re
 import csv
 
-genre = input('Какой жанр игр Вас интересует?\n').split(',')
-category = input('Какая категория игр Вас интересует?\n').split(',')
-developer = input('Игры какого разработчика Вас интересуют?\n').split(',')
-platform = input('На какой платформе вы собираетесь играть?\n').split(',')
+genre = input('Какой жанр игр Вас интересует?\n').capitalize().lstrip().split(',')
+category = input('Какая категория игр Вас интересует?\n').swapcase().lstrip().split(',')
+developer = input('Игры какого разработчика Вас интересуют?\n').swapcase().lstrip().split(',')
+platform = input('На какой платформе вы собираетесь играть?\n').lower().lstrip().split(',')
 
 year = (input('Какой год выхода Вас интересует? (Можно ввести промежуток)\n'))
 if '-' in year:
@@ -13,7 +13,7 @@ if '-' in year:
 else:
     period = False
 
-cost = input('Выберите допустимую цену игры (используйте < или >)\n')
+cost = input('Выберите допустимую цену игры в долларах (используйте < или >)\n')
 if cost == '':
     cost = [0.0, 422.0]
 elif cost[0] == '<':
@@ -22,7 +22,7 @@ elif cost[0] == '<':
 else:
     cost = [float(cost), float(cost)]
 
-ratings = input('Положительных отзывов должно быть больше, чем отрицательных? (Введите \'да\' или \'нет\')\n')
+ratings = input('Положительных отзывов должно быть больше, чем отрицательных? (Введите \'да\' или \'нет\')\n').lower()
 if ratings == 'да':
     good = True
 else:
@@ -43,7 +43,7 @@ with open('steam.csv', encoding='utf-8') as f, \
         else:
             date = (line[2].split('-')[0] == year) | (year == '')
 
-        if ((any(genre in line[9].split(';') for genre in genre) | (genre == [''])) and
+        if ((any(genre in line[9].split(';') and line[10].split(';') for genre in genre) | (genre == [''])) and
                 date and
                 (any(category in line[8].split(';') for category in category) | (category == [''])) and
                 (any(developer in line[4].split(';') for developer in developer) | (developer == [''])) and
